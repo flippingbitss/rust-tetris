a3f92ce7c7f88cdc151ba534ed730fd2e7d70d19
Manpreet Singh Matharu <matharumanpreet00@gmail.com>
2018-12-05 19:25:54 -0500

    impl random distribution for piecetype, added Game struct, block matrix generation and piece rendering

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+use rand::distributions::Standard;
+use rand::prelude::*;
 use sdl2::pixels::Color;
+use std::default::Default;
 
+#[derive(Debug)]
 pub enum GameColor {
     Red,
     Green,
@@ -8,6 +12,13 @@ pub enum GameColor {
     Cyan,
     Orange,
     Purple,
+    Gray,
+}
+
+impl Default for GameColor {
+    fn default() -> Self {
+        GameColor::Red
+    }
 }
 
 impl From<GameColor> for Color {
@@ -20,10 +31,12 @@ impl From<GameColor> for Color {
             GameColor::Cyan => Color::RGB(0, 255, 255),
             GameColor::Orange => Color::RGB(255, 165, 0),
             GameColor::Purple => Color::RGB(128, 0, 128),
+            GameColor::Gray => Color::RGB(25, 25, 25),
         }
     }
 }
 
+#[derive(Debug, Clone)]
 pub enum PieceType {
     J,
     L,
@@ -34,67 +47,128 @@ pub enum PieceType {
     O,
 }
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum Presence {
+    No,
+    Yes,
+}
+
+type PieceMatrix = [[Presence; 4]; 4];
+type GameMap = [Vec<Presence>];
+
+#[derive(Default)]
 pub struct Piece {
-    build: [u16; 4],
-    color: GameColor,
-    x: usize,
-    y: usize,
-    current_build: usize
+    pub states: [u16; 4],
+    pub color: GameColor,
+    pub x: isize,
+    pub y: usize,
+    pub current_state: usize,
+}
+
+impl Piece {
+    pub fn rotate(&mut self, game_map: &GameMap) {
+        let temp_state = (self.current_state + 1) % 4;
+        let x_pos_matches = [0, -1, 1, -2, 2, -3];
+
+        for x in x_pos_matches.iter() {
+            if self.test_position(&game_map, temp_state, *x as isize, self.y) {
+                self.current_state = temp_state;
+                self.x += *x;
+                break;
+            }
+        }
+    }
+
+    pub fn move_position(&mut self, game_map: &GameMap, new_x: isize, new_y: usize) -> bool {
+        if self.test_position(&game_map, self.current_state, new_x, new_y) {
+            self.x = new_x;
+            self.y = new_y;
+            return true;
+        }
+        false
+    }
+
+    pub fn get_block_matrix(&self, state: usize) -> PieceMatrix {
+        use self::Presence::*;
+        let num = self.states[state];
+        let mut res = [[No; 4]; 4];
+
+        for i in 0..16 {
+            if num & 1u16 << 15 - i > 0 {
+                res[i / 4][i % 4] = Yes
+            }
+        }
+        res
+    }
+
+    fn test_position(&self, game_map: &[Vec<Presence>], state: usize, x: isize, y: usize) -> bool {
+        let state_m = self.get_block_matrix(state);
+
+        for mx in 0..4 {
+            for my in 0..4 {
+                if state_m[my][mx] == Presence::Yes {
+                    if x as usize + mx >= game_map[y].len()
+                        || y + my >= game_map.len()
+                        || game_map[y + my][x as usize + mx] == Presence::Yes
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
 }
 
 impl From<PieceType> for Piece {
-    fn from(piece_type: PieceType) -> Self {
+    fn from(piece_type: PieceType) -> Piece {
+        use self::PieceType::*;
+
+        let def = Piece::default();
+
         match piece_type {
-            PieceType::L => Piece {
-                build: [17504, 736, 1570, 1856],
+            L => Piece {
+                states: [17504, 736, 1570, 1856],
                 color: GameColor::Orange,
-                x: 0,
-                y: 0,
-                current_build: 0
+                ..def
             },
-            PieceType::J => Piece {
-                build: [8800, 1136, 1604, 3616],
+            J => Piece {
+                states: [8800, 1136, 1604, 3616],
                 color: GameColor::Blue,
-                x: 0,
-                y: 0,
-                current_build: 0
+                ..def
             },
-            PieceType::S => Piece {
-                build: [17952, 1728, 17952, 1728],
+            S => Piece {
+                states: [17952, 1728, 17952, 1728],
                 color: GameColor::Green,
-                x: 0,
-                y: 0,
-                current_build: 0
+                ..def
             },
-            PieceType::Z => Piece {
-                build: [9792, 3168, 9792, 3168],
+            Z => Piece {
+                states: [9792, 3168, 9792, 3168],
                 color: GameColor::Red,
-                x: 0,
-                y: 0,
-                current_build: 0
+                ..def
             },
-            PieceType::T => Piece {
-                build: [17984, 3648, 17984, 3648],
+            T => Piece {
+                states: [17984, 3648, 17984, 3648],
                 color: GameColor::Purple,
-                x: 0,
-                y: 0,
-                current_build: 0
+                ..def
             },
-            PieceType::I => Piece {
-                build: [17476, 3840, 17476, 3840],
+            I => Piece {
+                states: [17476, 3840, 17476, 3840],
                 color: GameColor::Cyan,
-                x: 0,
-                y: 0,
-                current_build: 0
+                ..def
             },
-            PieceType::O => Piece {
-                build: [1632, 1632, 1632, 1632],
+            O => Piece {
+                states: [1632, 1632, 1632, 1632],
                 color: GameColor::Yellow,
-                x: 0,
-                y: 0,
-                current_build: 0
+                ..def
             },
         }
     }
 }
 
+impl Distribution<PieceType> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> PieceType {
+        use self::PieceType::*;
+        rng.choose(&[L, J, S, Z, T, I, O]).unwrap().clone()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate sdl2;
 
 mod lib;
 
+use rand::prelude::*;
+
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
@@ -13,16 +15,38 @@ use sdl2::Sdl;
 use std::thread::sleep;
 use std::time::Duration;
 
-use self::lib::{GameColor, Piece, PieceType};
+use crate::lib::{GameColor, Piece, PieceType, Presence};
 
 // constants
 const TITLE: &'static str = "Tetris in Rust";
 
 const NUM_BLOCKS_X: u32 = 10;
 const NUM_BLOCKS_Y: u32 = 18;
+
+const BORDER_WIDTH: u32 = 1;
 const TEXTURE_SIZE: u32 = 32;
+const TEXTURE_SIZE_INNER: u32 = TEXTURE_SIZE - BORDER_WIDTH * 2;
+
 const WIDTH: u32 = NUM_BLOCKS_X * TEXTURE_SIZE; // 480;
 const HEIGHT: u32 = NUM_BLOCKS_Y * TEXTURE_SIZE; //860;
+
+struct Game {
+    current_piece: Piece,
+    score: usize,
+    lines_cleared: usize,
+    game_map: Vec<Vec<Presence>>
+}
+
+impl Game {
+    fn new() -> Game {
+        Game {
+            current_piece: Piece::from(random::<PieceType>()),
+            score: 0,
+            lines_cleared: 0,
+            game_map: vec![vec![Presence::No; NUM_BLOCKS_X as usize]; NUM_BLOCKS_Y as usize]
+        }
+    }
+}
 
 // initialize sdl context and canvas
 fn main() {
@@ -47,39 +71,82 @@ fn main() {
         .expect("Failed to build canvas");
 
     let texture_creator = canvas.texture_creator();
-    let square_texture =
-        create_square_texture(&mut canvas, &texture_creator).expect("Failed to create texture");
+
+    macro_rules! texture {
+        ($color: expr) => {
+            create_texture_rect(&mut canvas, &texture_creator, $color)
+                .expect("Failed to create texture");
+        };
+    }
 
     // set canvas background and clear it
-    canvas.set_draw_color(Color::from(GameColor::Red));
+    canvas.set_draw_color(GameColor::Blue);
     canvas.clear();
 
-    // copy the texture onto the canvas and draw
-    canvas
-        .copy(
-            &square_texture,
-            None,
-            Rect::new(0, 0, TEXTURE_SIZE, TEXTURE_SIZE),
-        )
-        .unwrap();
+    let textures = [texture!(GameColor::Red), texture!(GameColor::Gray)];
 
 
 
+    let mut game = Game::new();
+
+    draw_tetris_piece(&mut canvas, &textures, &game);
+
+    //    println!("{:?}", format!("{:b}",curr).split(""));
+    //    println!("{:?}", mat);
     canvas.present();
 
-    start_render_loop(&sdl_context)
+    start_render_loop(&sdl_context, &mut game)
 }
 
-fn create_square_texture<'a>(
+fn draw_tetris_piece(
+    canvas: &mut Canvas<Window>,
+    textures: &[Texture; 2],
+    game: &Game
+) {
+    let piece = &game.current_piece;
+    let mat = piece.get_block_matrix(piece.current_state);
+    let [block_tex, border_tex] = textures;
+    let x = piece.x;
+    let y = piece.y;
+
+    for row in 0..4 {
+        for col in 0..4 {
+            if mat[row][col] == Presence::Yes {
+                let y_offset = (y + row * TEXTURE_SIZE as usize) as i32;
+                let x_offset = (x as usize + col * TEXTURE_SIZE as usize) as i32;
+
+                canvas.copy(
+                    &border_tex,
+                    None,
+                    Rect::new(y_offset, x_offset, TEXTURE_SIZE, TEXTURE_SIZE),
+                );
+
+                canvas.copy(
+                    &block_tex,
+                    None,
+                    Rect::new(
+                        y_offset + BORDER_WIDTH as i32,
+                        x_offset + BORDER_WIDTH as i32,
+                        TEXTURE_SIZE_INNER,
+                        TEXTURE_SIZE_INNER,
+                    ),
+                );
+            }
+        }
+    }
+}
+
+fn create_texture_rect<'a>(
     canvas: &mut Canvas<Window>,
     texture_creator: &'a TextureCreator<WindowContext>,
+    texture_color: GameColor,
 ) -> Option<Texture<'a>> {
     let result = texture_creator.create_texture_target(None, TEXTURE_SIZE, TEXTURE_SIZE);
 
     if let Ok(mut square_texture) = result {
         canvas
             .with_texture_canvas(&mut square_texture, |texture| {
-                texture.set_draw_color(Color::from(GameColor::Blue));
+                texture.set_draw_color(texture_color);
                 texture.clear();
             })
             .expect("Failed texture drawing");
@@ -90,7 +157,7 @@ fn create_square_texture<'a>(
     }
 }
 
-fn start_render_loop(sdl_context: &Sdl) {
+fn start_render_loop(sdl_context: &Sdl, game: &mut Game) {
     let mut event_pump = sdl_context
         .event_pump()
         .expect("Failed to get sdl context event pump");
